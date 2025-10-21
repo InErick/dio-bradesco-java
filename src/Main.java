18eeea104ec4a202c82f916c41224ff9589499bb
Erick Ribeiro <ericktho202@gmail.com>
2025-10-20 21:39:13 -0300

    Primeiro dia

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Saaalve");
+        String oi ="oi";
+        System.out.println("argumento 1:"+args[0]);
+        System.out.println("argumento 2:"+args[1]);
+    }
+}
